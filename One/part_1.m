ecb9eb521e587ef3fd52391fe8c560c013c6cfa9
Thomas Lim <txl11@ic.ac.uk>
2014-10-24 02:07:18 +0100

    Inroads in to part 2

--- One/part_1.m
+++ One/part_1.m
@@ -1,5 +1,5 @@
 
-ORIG_IMG = 'cat.jpg';
+ORIG_IMG = 'rah.jpg';
 
 % Part one
 
@@ -67,7 +67,7 @@ while i<=10,
 end
 
 
-% 5: 
+% 5: resizing and 
 OUTPUT_DIM = 20;
 
 uniform_squares = cell(10,1);
@@ -76,13 +76,66 @@ for i=1:10,
     [dim, ~] = size(squares{i});
     factor = OUTPUT_DIM/dim;
     uniform_squares{i} = imresize(squares{i}, factor);
-    figure;
-    imshow(uniform_squares{i})
-    
-%     im_combined(:,i) = uint8(reshape(uniform_squares{i}, 1, 400));
+%     figure;
+%     imshow(uniform_squares{i})
+        
+% The reshape function also works    
+% 	im_combined(:,i) = uint8(reshape(uniform_squares{i}, 1, 400));
 
     im_combined(:,i) = uniform_squares{i}(:);
 end
 
 figure;
 imshow(im_combined)
+
+
+% Part 2, 1: 100 random squares
+NUM_RANDOMS = 100;
+OUTPUT_DIM = 20;
+
+i = 1;
+more_squares = cell(10,1);
+while i<=NUM_RANDOMS,
+    square = round(rand*width);
+    xstart = round(rand*width);
+    ystart = round(rand*height);
+    if xstart + square > width || ystart + square > height
+        continue
+    else
+%         figure;
+        more_squares{i} = imcrop(im_grey, [xstart, ystart square square]);
+%         imshow(more_squares{i})
+%         filename = sprintf('sq_img_%d.jpg', i);
+%         imwrite(more_squares{i}, filename)
+        i = i + 1;
+    end
+end
+
+
+more_uniform_squares = cell(NUM_RANDOMS,1);
+larger_im_combined = uint8(zeros(400, NUM_RANDOMS));
+for i=1:NUM_RANDOMS,
+    [dim, ~] = size(more_squares{i});
+    factor = OUTPUT_DIM/dim;
+    more_uniform_squares{i} = imresize(more_squares{i}, factor);
+%     figure;
+%     imshow(uniform_squares{i})
+        
+% The reshape function also works    
+% 	im_combined(:,i) = uint8(reshape(uniform_squares{i}, 1, 400));
+
+    larger_im_combined(:,i) = more_uniform_squares{i}(:);
+end
+
+figure;
+imshow(larger_im_combined)
+
+
+
+% 2: mean vector and covariance matrix
+
+% Mean:
+mean_vector = sum(larger_im_combined)/400;
+
+% Covariance Matrix:
+
