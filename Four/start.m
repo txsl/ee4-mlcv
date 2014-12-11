693b0163f7cd9a45cfd1e64e6f109dbd736b1da8
Thomas <txl11@imperial.ac.uk>
2014-12-11 23:13:05 +0000

    Started working on section properly. Kinda...

--- Four/start.m
+++ Four/start.m
@@ -4,12 +4,41 @@ load face;
 
 IM_WIDTH = 46;
 IM_HEIGHT = 56;
+IMS_PER_FACE = 10;
 
-[ distrib, bin_num ] = histc(l, unique(l));
+num_faces = size(unique(l), 2);
 
-for i=1:size(X,2)
-    figure
-    title(l(i))
-    im_c = reshape(uint8(X(:,i)), [ IM_HEIGHT IM_WIDTH ]);
-    imshow(im_c)
+% [ distrib, ~ ] = histc(l, num_faces);
+
+% for i=1:size(X,2)
+%     figure
+%     title(l(i))
+%     im_c = reshape(uint8(X(:,i)), [ IM_HEIGHT IM_WIDTH ]);
+%     imshow(im_c)
+%     pause
+% end
+
+%% Q2
+
+te = cell(num_faces, 1);
+tr = te;
+
+all_tr = []
+for i=1:num_faces
+    tr{i} = X(:,(10*(i-1))+1:(10*(i-1))+8);
+    te{i} = X(:,(10*(i-1))+9:(10*(i-1))+10);
+    all_tr = [ all_tr; tr{i} ];
+end
+
+%% Q3
+
+diff = cell(num_faces, 1);
+A = diff;
+for i=1:num_faces
+    diff{i} = tr{i} - repmat(mean(tr{i}, 2), 1, 8);
+    A{i} = cell(8,1);
+    for j=1:8
+        A{i}{j} = diff{i}(:,j) * diff{i}(:,j)';
+        [v, e] = eig(A{i}{j});
+    end
 end
