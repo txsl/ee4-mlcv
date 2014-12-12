339af6ce54d75d8ace856a07d65ff4812fd7cf4f
Thomas <txl11@imperial.ac.uk>
2014-12-12 19:45:34 +0000

    Leeetle more progress on cwk4

--- Four/start.m
+++ Four/start.m
@@ -18,27 +18,32 @@ num_faces = size(unique(l), 2);
 %     pause
 % end
 
+
 %% Q2
 
 te = cell(num_faces, 1);
 tr = te;
 
-all_tr = []
+all_tr = [];
 for i=1:num_faces
     tr{i} = X(:,(10*(i-1))+1:(10*(i-1))+8);
     te{i} = X(:,(10*(i-1))+9:(10*(i-1))+10);
-    all_tr = [ all_tr; tr{i} ];
+    all_tr = [ all_tr tr{i} ];
 end
+
 
 %% Q3
 
-diff = cell(num_faces, 1);
-A = diff;
-for i=1:num_faces
-    diff{i} = tr{i} - repmat(mean(tr{i}, 2), 1, 8);
-    A{i} = cell(8,1);
-    for j=1:8
-        A{i}{j} = diff{i}(:,j) * diff{i}(:,j)';
-        [v, e] = eig(A{i}{j});
-    end
-end
+% diff = cell(num_faces, 1);
+% A = diff;
+% for i=1:num_faces
+%     diff{i} = tr{i} - repmat(mean(tr{i}, 2), 1, 8);
+%     A{i} = cell(8,1);
+%     for j=1:8
+%         A{i}{j} = diff{i}(:,j) * diff{i}(:,j)';
+%         [v, e] = eig(A{i}{j});
+%     end
+% end
+
+cov(all_tr')  % Since each column is an observation, and each row is a variable, and each column an observation
+
